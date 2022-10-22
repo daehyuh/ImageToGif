33d9f9c189e4d8698e757d692629dfd06df05c83
대현 <rkdeown10@naver.com>
2022-10-22 16:37:41 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
+from PIL import Image
+import matplotlib.image as mpimg
+path = [f"./complete_demo/{i}" for i in os.listdir("./complete_demo")]
+paths = [Image.open(i) for i in path]
+imageio.mimsave('./complete_demo.gif', paths, fps=5
